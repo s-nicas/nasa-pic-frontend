d25a6cd222eea20b896f90e00a9d6ed78f80953e
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 11:31:51 -0800

    Add map state to props and map dispatch to props

--- src/containers/PhotosContainer.js
+++ src/containers/PhotosContainer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import SearchInput from './SearchInput'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-class PhotosConatiner extends Component {
+class PhotosContainer extends Component {
   render(){
     return (
       <div>
       </div>
-
     )
   }
 }
+
+
+const mapStateToProps = state => ({
+   photo: state.pictures
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchPhoto: date => dispatch({type: 'FETCH_PHOTO', date})
+})
+export default connect(mapStateToProps, mapDispatchToProps)(PhotosContainer)
