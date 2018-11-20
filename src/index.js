2631c21ca3220a4aa69db21c3926f6cdf15c0065
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 10:30:17 -0800

    Add installed redux and react-redux
    Few changes - changed service worker back to previous state and updated register to serviceWorker.register(). Update create store to applymiddleware thunk - had an incorrect argument

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
@@ -13,14 +13,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(ReduxThunk))
+  applyMiddleware(thunk))
 
-ReactDOM.render(
-  <Provider store={store} />
-    <App />
-  </Provider>, document.getElementById('root'));
-
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  document.getElementById('root')
+  )
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-registerServiceWorker();
+serviceWorker.register()
