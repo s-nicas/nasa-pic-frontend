ccfcb4fee36a8852f622a057f777def7421962fa
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-19 16:47:43 -0800

    Add search input container
    will hold search form and handle user input

---
+++ src/containers/SearchInput.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+
+class SearchInput extends Component {
+  state ={
+    date: ''
+  }
+
+
+  render(){
+    return (
+
+    )
+  }
+}
+
+export default SearchInput
