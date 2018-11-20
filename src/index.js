7f8fc8b5966bf1dcc5f3111a2595eda1e534a527
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 09:48:57 -0800

    Add thunk, provider, create store, applyMiddleware, rootreducer
    Also created store and add provider to index.js. Changed service worker to register so app can work offline

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/rootReducer'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(ReduxThunk))
+
+ReactDOM.render(
+  <Provider store={store} />
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
