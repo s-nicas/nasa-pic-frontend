79a6a4334b80477ba6519b079bafbfcbb5b47f85
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 10:44:56 -0800

    Add photosConatiner

---
+++ src/containers/PhotosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import SearchInput from './SearchInput'
+import {connect} from 'react-redux'
+
+class PhotosConatiner extends Component {
+  render(){
+    return (
+      <div>
+      </div>
+
+    )
+  }
+}
