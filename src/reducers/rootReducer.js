8eb19cc5f43da16a672bb531291e48bde0ae2c9c
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 16:15:54 -0800

    Add fetch photo and add_photo to state
    tested and working

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,11 +4,9 @@ export default function rootsReducer(state = {
   }, action) {
     switch (action.type) {
       case 'FETCH_PHOTO':
-        return action.images
-       //  return {...state, isFetchingPhoto: true, pictures: []}
-       // case 'ADD_PHOTO_TO_STATE':
-       //  return state
-
+        return {...state, isFetchingPhoto: true, pictures: []}
+       case 'ADD_PHOTO_TO_STATE':
+        return {isFetchingPhoto: false, pictures: action.data}
        default:
         return state;
     }
