b9ab497bc6b90af280bd4267f76a60beb5877269
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 17:43:16 -0800

    Add passing props into photo
    tested and working

--- src/containers/PhotoInfo.js
+++ src/containers/PhotoInfo.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
+import Photo from './Photo'
 
 
 const PhotoInfo = props => {
-  
-  return ('test')
+  if (props.photoInfo){
+    const renderPhoto = (<Photo key={1} mediaType={props.photoInfo.media_type} title={props.photoInfo.title} url={props.photoInfo.url} explanation={props.photoInfo.explanation}/>)
+
+    return (
+      <div>
+        {renderPhoto}
+      </div>
+    )
+  }
+  return (<div></div>)
+
 }
 
 export default PhotoInfo
