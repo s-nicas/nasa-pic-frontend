d3ebfe7797da692cb7ef46dbd9b7b26f3b3e4648
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-21 11:05:03 -0800

    Delete react styling app and header
    updated to fix app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import PhotosContainer from './containers/PhotosContainer'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import NavBar from './containers/Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <PhotosContainer/>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" exact component={PhotosContainer}/>
+          <header className="App-header">
+
+           </header>
+        </div>
+      </Router>
     );
   }
 }
