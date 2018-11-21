aa396050698776188410b0e5b1faf7917538ceb9
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 20:15:05 -0800

    Add className center for picture styling add weidth and heigh on photo
    Add center css styling. Also add bootstrap and info to photo.js - not used right now

--- src/containers/Photo.js
+++ src/containers/Photo.js
@@ -1,23 +1,26 @@
 import React, {Component} from 'react'
+import Info from './Info'
+import { Button } from 'reactstrap';
 
 class Photo extends Component {
 
   mediaType = (props) =>{
     if(props.mediaType === "video"){
-      return (<iframe width="420" height="315"
-        src={props.url}>
-      </iframe>)
+      return (
+        <iframe width="560" height="400"
+          src={props.url} title={this.props.title}>
+        </iframe>
+      )
     } else {
-    return (<img src={this.props.url} alt={this.props.title}/>)
+    return (<img className="center" src={this.props.url} alt={this.props.title} />)
     }
   }
 
   render(){
     return (
-
       <div>
-       {this.props.title}
-       {this.mediaType(this.props)}
+         {this.props.title}
+         {this.mediaType(this.props)}
       </div>
     )
   }
