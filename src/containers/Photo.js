ac882d4bff071f928892f38ca802cd2d01e81e24
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 18:36:12 -0800

    Add mediaType function
    will render video if video or picture if image
    
    testing and working but format is off

--- src/containers/Photo.js
+++ src/containers/Photo.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react'
 
 class Photo extends Component {
 
+  mediaType = (props) =>{
+    if(props.mediaType === "video"){
+      return (<iframe width="420" height="315"
+        src={props.url}>
+      </iframe>)
+    } else {
+    return (<img src={this.props.url} alt={this.props.title}/>)
+    }
+  }
+
   render(){
     return (
 
       <div>
        {this.props.title}
-       <img src={this.props.url} alt={this.props.title}/>
+       {this.mediaType(this.props)}
       </div>
     )
   }
