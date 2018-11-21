b74af9c6e4a6ebda880f74cdb50ebdfb2bd9e3ed
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-20 16:21:00 -0800

    Fix fetch photo function
    tested and working

--- src/actions/picActions.js
+++ src/actions/picActions.js
@@ -1,17 +1,9 @@
 export function fetchPhoto() {
-  const images =  fetch(`https://api.nasa.gov/planetary/apod?&hd=false&api_key=s773cGTT3VGPiJQZ9Hx0I1l4Nv07JiihqIYQohKf`)
-  return {
-    type: 'FETCH_PHOTO',
-    images
+  return (dispatch) => {
+    dispatch({type: 'FETCH_PHOTO'});
+    fetch(`https://api.nasa.gov/planetary/apod?&hd=false&api_key=[insert key]`)
+    .then(response => response.json())
+    .then(data => dispatch({type: 'ADD_PHOTO_TO_STATE', data: data}))
   }
+
 }
-// I hard coded the route to test it for now
-
-
-
-// return (dispatch) => {
-//   dispatch({type: 'FETCH_PHOTO'});
-//   fetch(`https://api.nasa.gov/planetary/apod?date=${date}&hd=false&api_key=s773cGTT3VGPiJQZ9Hx0I1l4Nv07JiihqIYQohKf`)
-//   .then(response => response.json())
-//   .then(data => dispatch({type: 'ADD_PHOTO_TO_STATE', data: data}))
-// }
