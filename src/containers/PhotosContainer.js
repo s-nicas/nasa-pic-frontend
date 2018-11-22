11ce5a90ddd2bae9512a34e044332133408617ba
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-22 10:22:39 -0800

    Add conditional statement to PhotosContainer return
    If photo hasn't been saved / fetched will not render PhotoInfo or PictureInfo

--- src/containers/PhotosContainer.js
+++ src/containers/PhotosContainer.js
@@ -3,13 +3,16 @@ import SearchInput from './SearchInput'
 import { connect } from 'react-redux'
 import { fetchPhoto } from '../actions/picActions'
 import PhotoInfo from './PhotoInfo'
+import PictureInfo from './Info'
 
 class PhotosContainer extends Component {
+
   render(){
     return (
       <div>
         <SearchInput fetchPhoto={this.props.fetchPhoto}/>
-        <PhotoInfo photoInfo={this.props.photo} />
+        {Object.keys(this.props.photo).length !== 0 ? <PhotoInfo photoInfo={this.props.photo} /> : true }
+        {Object.keys(this.props.photo).length !== 0 ? <PictureInfo photo={this.props.photo}/> : false}
       </div>
     )
   }
