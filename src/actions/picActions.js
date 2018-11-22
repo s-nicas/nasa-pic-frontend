b7a7432f1fdeb799b51c6102666ec7a29ad3e7dc
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-21 18:19:46 -0800

    Add date into fetch request, also added method of "GET"
    was returning the header without the Method.

--- src/actions/picActions.js
+++ src/actions/picActions.js
@@ -1,11 +1,12 @@
 export function fetchPhoto(date) {
-  const url = `https://api.nasa.gov/planetary/apod?date=${date}&hd=false&api_key=s773cGTT3VGPiJQZ9Hx0I1l4Nv07JiihqIYQohKf`
+  // const url = `https://api.nasa.gov/planetary/apod?date=${date}&hd=false&api_key=s773cGTT3VGPiJQZ9Hx0I1l4Nv07JiihqIYQohKf`
 
   return (dispatch) => {
     dispatch({type: 'FETCH_PHOTO'});
-    fetch(url)
+    fetch(`http://localhost:3000/pictures/${date}`){
+      method: 'GET',
     .then(response => response.json())
     .then(data => dispatch({type: 'ADD_PHOTO_TO_STATE', data: data}))
   }
-
+}
 }
