45fbbe41b07b17d76c2db0cda7e7fcfd407176f1
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-22 12:39:10 -0800

    Add Home file which will work as the route page
    Not implemented yet. Pulled a photo from Nasa which will be the root photo

---
+++ src/containers/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+const Home = () => {
+
+    return (
+      <div className="center">
+        <img src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/atmosphere_geo5_2018235_eq.jpg" alt='Just Another Day in Aerosol Earth' />
+      </div>
+    )
+
+
+}
+export default Home
