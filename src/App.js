1d53fa8c35b9cacddf27f9bae46b17b0bfce8b17
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-22 10:36:27 -0800

    Add route and browser router from react - router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import PhotosContainer from './containers/PhotosContainer'
+import { BrowserRouter }  from 'react-router-dom'
+import Route from 'react-router-dom/Route'
 
 class App extends Component {
   render() {
