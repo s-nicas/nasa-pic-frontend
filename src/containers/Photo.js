6535247270d8288012eb5cd60b026a871bfdf3a1
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-23 15:30:56 -0800

    Add styling to home photo
    deleted react default stayling in app.css

--- src/containers/Photo.js
+++ src/containers/Photo.js
@@ -1,32 +1,26 @@
 import React, {Component} from 'react'
-import Info from './Info'
-import { Button } from 'reactstrap';
 
 class Photo extends Component {
 // how to render info upon click
-  handleOnClick = () =>{
-    return <Info key={1} explanation={this.props.explanation}/>
-  }
 
   mediaType = (props) =>{
     if(props.mediaType === "video"){
       return (
-        <iframe width="560" height="400"
+        <iframe className="Home-Pic" width="560" height="400"
           src={props.url} title={this.props.title}>
         </iframe>
       )
     } else {
-    return (<img className="center" src={this.props.url} alt={this.props.title} />)
+    return (<img className="Home-Pic" src={this.props.url} alt={this.props.title} width="auto"/>)
     }
   }
 
 // TODO not sure how to have button / page render only if a photo is available
   render(){
     return (
-      <div>
+      <div >
          {this.props.title}
          {this.mediaType(this.props)}
-         <Button outline color="primary" onClick={this.handleOnClick}>primary</Button>
       </div>
     )
   }
