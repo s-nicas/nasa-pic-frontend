8e96cbfde712a8eee55dd8d513c4edf3a1ecd323
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-23 15:12:40 -0800

    Update search bar with react bootstrap with material design.
    broke away search bar from Nav bar. Also moved photos container map state to props to search

--- src/containers/SearchInput.js
+++ src/containers/SearchInput.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl, Button } from 'react-bootstrap'
+import { fetchPhoto } from '../actions/picActions'
+import { connect } from 'react-redux'
+import { Col, Fa } from "mdbreact";
 
 class SearchInput extends Component {
+   state = {date: ''};
 
-  constructor(props) {
-   super(props);
-   this.state = {date: ''};
- }
 
   handleOnChange = (event) => {
     this.setState({
@@ -23,29 +23,33 @@ class SearchInput extends Component {
      })
   }
 
-
-
-
 // TODO need to update date so that they can't pick future dates
   render(){
     return (
-      <div className='header'>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/"><img src="https://api.nasa.gov/images/logo.png" alt="NASA logo" width="50" height="50"/>Image of the Day</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-              <FormGroup >
-                <FormControl type="date"  max="2018-11-22" value={this.state.date} onChange={(event) => this.handleOnChange(event)}/>
-              </FormGroup>{' '}
-              <Button type="submit" onClick={(event) => this.handleOnSubmit(event)}>Search</Button>
-        </Navbar >
-
-
+      <div >
+        <Col md="6">
+          <form className="form-inline mt-4 mb-4">
+            <Fa icon="search" />
+            <input
+              className="form-control form-control-sm ml-3 w-75"
+              type="date"
+              value={this.state.date}
+              onChange={(event) => this.handleOnChange(event)}
+            />
+            <Button type="submit" onClick={(event) => this.handleOnSubmit(event)}>Search</Button>
+          </form>
+        </Col>
       </div>
     )
   }
 }
 
-export default SearchInput
+
+function mapDispatchToProps(dispatch){
+  return { fetchPhoto: date => dispatch(fetchPhoto(date)) }
+}
+
+function mapStateToProps(state){
+  return {photo: state.pictures}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SearchInput)
