a6d6edb79b90cee3b8700f9eded01915c013c4f6
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-23 14:24:59 -0800

    Add fetchTodaysPhoto to actions
    Todays photo will call on local host route page to call todays photo. Seperated code from fetch photo - easier to debug should one functionality break

--- src/actions/picActions.js
+++ src/actions/picActions.js
@@ -2,8 +2,22 @@ export function fetchPhoto(date) {
 
   return (dispatch) => {
     dispatch({type: 'FETCH_PHOTO'});
-    fetch(`http://localhost:3000/pictures/${date}`,{method: 'GET'})
+    fetch(`http://localhost:3001/pictures/${date}`,{
+      method: 'GET'})
     .then(response => response.json())
     .then(data => dispatch({type: 'ADD_PHOTO_TO_STATE', data: data}))
   }
+
+}
+
+
+export function fetchTodaysPhoto() {
+  return (dispatch) => {
+    dispatch({type: 'FETCH_TODAYS_PHOTO'});
+    fetch(`http://localhost:3001`,{
+      method: 'GET'})
+    .then(response => response.json())
+    .then(data => dispatch({type: 'ADD_PHOTO_TO_STATE', data: data}))
+  }
+
 }
