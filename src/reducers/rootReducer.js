2a6c205d9e54903a90b0c10f2a7cee2ceda4bbed
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-24 16:04:18 -0800

    Add FETCH_MARS_PHOTOS and ADD_MARS_PHOTO_TO_STATE
    fetch mars photos change is fetching photo to true
    add mars photo to state - adds photo to pictures array and changes fetching photo back to false

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -7,6 +7,10 @@ export default function rootsReducer(state = {
         return {...state, isFetchingPhoto: true, pictures: []}
        case 'ADD_PHOTO_TO_STATE':
         return {isFetchingPhoto: false, pictures: action.data}
+       case 'FETCH_MARS_PHOTOS':
+        return {isFetchingPhoto: true, pictures: []}
+       case 'ADD_MARS_PHOTO_TO_STATE':
+        return {isFetchingPhoto: false, pictures action.data}
        default:
         return state;
     }
