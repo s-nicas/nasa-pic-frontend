5cafaee0e0a6e4890ac2a03caaf93d99d08e35af
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-24 16:11:47 -0800

    Add MapDispatchToProps and mapStateToProps
    Also - importing fetchMarsPhotos from actions file and created new file called Mars Pictures - mars pictures will loop through all the data received, extract what is needed and pass it to MarsPicture - which will display each photo.
    
    Mars picturs not create yet

--- src/containers/MarsContainer.js
+++ src/containers/MarsContainer.js
@@ -1,1 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchMarsPhotos } from '../actions/picActions'
 
+class MarsContainer extends Component {
+
+  render(){
+    return(
+
+
+    )
+  }
+}
+
+  function mapDispatchToProps(dispatch){
+    return { fetchMarsPhotos: () => dispatch(fetchMarsPhotos()) }
+  }
+
+  function mapStateToProps(state){
+    return {photos: state.pictures}
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(MarsContainer)
