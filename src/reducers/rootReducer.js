1beb58b48414f0bc2bd74785b75306e6fe5467c7
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-24 16:32:45 -0800

    Fix syntax error
    'ADD_MARS_PHOTO_TO_STATE' pictures was missing colon

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -10,7 +10,7 @@ export default function rootsReducer(state = {
        case 'FETCH_MARS_PHOTOS':
         return {isFetchingPhoto: true, pictures: []}
        case 'ADD_MARS_PHOTO_TO_STATE':
-        return {isFetchingPhoto: false, pictures action.data}
+        return {isFetchingPhoto: false, pictures: action.data}
        default:
         return state;
     }
