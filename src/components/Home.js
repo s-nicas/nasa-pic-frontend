8bdec1b311ff376ac192351e54de908ebdaf930f
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-25 18:48:06 -0800

    Delete handle redirect
    Removed search button - so handle redirect is no longer needed. Search function is now Daily Photo Archive link

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,24 +18,14 @@ class Home extends Component {
     })
   }
 
-  handleRedirect = () => {
-    if (this.state.redirect){
-      return <Redirect to='/search'/>
-    }
-  }
-
-
-
   componentDidMount(){
     this.props.fetchTodaysPhoto();
   }
+
     render(){
-
-
       return(
       <div>
         <PhotoInfo photoInfo={this.props.photo} />
-        {this.handleRedirect()}
         <InformationPopOver photo={this.props.photo}/>
       </div>
       )
