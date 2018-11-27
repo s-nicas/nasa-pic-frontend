0841358dbf7c962fe4ec1974d174a9b13d0e48b6
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-27 14:26:43 -0800

    Add bootstrap button
    Add state with visible. Along with functionality components. Open comment box - close will close box.
    
    tested and working

---
+++ src/components/CommentsInput.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Modal from 'react-awesome-modal';
+import { Button } from 'reactstrap';
+
+
+export default class CommentsInput extends Component {
+
+      state = {
+        visible : false
+      }
+
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
+
+    render() {
+        return (
+            <section id='button'>
+                <Button className="buttonComment" onClick={() => this.openModal()}>Comment</Button>
+                <Modal
+                    visible={this.state.visible}
+                    width="400"
+                    height="300"
+                    effect="fadeInUp"
+                    onClickAway={() => this.closeModal()}
+                >
+                    <div>
+                        <h1 >testjfjfjfjf</h1>
+                        <p >Some Contents</p>
+                        <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                    </div>
+                </Modal>
+            </section>
+        );
+    }
+}
