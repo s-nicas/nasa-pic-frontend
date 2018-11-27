3ed5ea22a334dc71b47c0125cdc16ec576c237df
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-27 14:32:00 -0800

    Add comment component

--- src/containers/PhotosContainer.js
+++ src/containers/PhotosContainer.js
@@ -3,14 +3,16 @@ import { connect } from 'react-redux'
 import { fetchPhoto } from '../actions/picActions'
 import PhotoInfo from '../components/PhotoInfo'
 import InformationPopOver from '../components/InformationPopOver'
+import CommentsInput from '../components/CommentsInput'
 
 class PhotosContainer extends Component {
 
   render(){
     return (
       <div>
-        {Object.keys(this.props.photo).length !== 0 ? <PhotoInfo photoInfo={this.props.photo} /> : true }
+        {Object.keys(this.props.photo).length !== 0 ? <PhotoInfo photoInfo={this.props.photo} /> : false }
         {Object.keys(this.props.photo).length !== 0 ? <InformationPopOver photo={this.props.photo}/> : false}
+        {Object.keys(this.props.photo).length !==0 ? <CommentsInput/> : false }
       </div>
     )
   }
