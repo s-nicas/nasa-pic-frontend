4892964a224ba168e40074144278dc77dc68a3c2
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-27 23:01:47 -0800

    Add files to build all pictures slide capability
    PicSlider is parent , slide, left/right arros are chidren

---
+++ src/components/PicSlider.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Slide from '../slide'
+import LeftArrow from '../left-arrow'
+import RightArrow from '../right-arrow'
+
+export default class PicSlider extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="slider">
+
+      </div>
+    );
+  }
+}
