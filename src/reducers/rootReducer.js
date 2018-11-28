41c51dcd0f799e11bab1f290c594392ce120cb19
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 13:57:31 -0800

    Add comments to add_photo_to_state and Add_Mars_photos
    tested and working

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 export default function rootsReducer(state = {
     isFetchingPhoto: false,
     pictures: [],
+    comments: [],
     commentRequestInProgress: false
     // comments: [],
     // userRequestInProgress: false,
@@ -13,17 +14,20 @@ export default function rootsReducer(state = {
       case 'FETCH_PHOTO':
         return {...state, isFetchingPhoto: true, pictures: []}
        case 'ADD_PHOTO_TO_STATE':
-        return {isFetchingPhoto: false, pictures: action.data}
+        return {isFetchingPhoto: false, pictures: action.data, comments: action.data.comments}
        case 'FETCH_MARS_PHOTOS':
         return {isFetchingPhoto: true, pictures: []}
        case 'ADD_MARS_PHOTO_TO_STATE':
-        return {isFetchingPhoto: false, pictures: action.data}
+        return {isFetchingPhoto: false, pictures: action.data, comments:[]}
         case 'BEGIN_USER_REQUEST':
          return {...state, userRequestInProgress: true}
         case 'LOGIN':
           return { loggedIn: true, currentUser: action.payload.user }
         case 'COMMENT_REQUEST':
             return {...state, commentRequestInProgress: true}
+        case 'ADD_COMMENT_TO_STATE':
+        const comment = action.data
+        return {...state, commentRequestInProgress: false, comments: state.comments.concat(comment) }
        default:
         return state;
     }
