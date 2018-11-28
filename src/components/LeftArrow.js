438c36b4ffb8fb6aa0eb29e575517c714ce9c708
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-27 23:07:51 -0800

    Add props to left arrow and right arrow form parent

--- src/components/LeftArrow.js
+++ src/components/LeftArrow.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const LeftArrow = () => {
-  return (
-    <div className="backArrow">
-      <i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i>
-    </div>
-  );
-}
+
+const LeftArrow = (props) => {
+    return (
+      <div className="backArrow" onClick={props.goToPrevSlide}>
+        <i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i>
+      </div>
+    );
+  }
 
 export default LeftArrow;
--- src/components/PicSlider.js
+++ src/components/PicSlider.js
@@ -5,16 +5,27 @@ import RightArrow from '../right-arrow'
 
 export default class PicSlider extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {}
   }
 
+  goToPrevSlide = () => {
+
+  }
+
+  goToNextSlide = () => {
+
+}
+
   render() {
     return (
       <div className="slider">
+      <Slide />
 
-      </div>
+      <LeftArrow goToPrevSlide={this.goToPrevSlide}/>
+      <RightArrow goToNextSlide={this.goToNextSlide}/>
+    </div>
     );
   }
 }
