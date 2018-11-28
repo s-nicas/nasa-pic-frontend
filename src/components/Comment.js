18837a6e847b4c0544ef3388f95f9a03b3b7eff8
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 14:57:49 -0800

    Add comment const
    use an arrow function - no logic pure presentational. Rendering each individual comment. Tested and working

---
+++ src/components/Comment.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+
+const Comment = (props) => {
+  return (
+      <div>
+          <p>{props.content}</p>
+      </div>
+    )
+  }
+
+export default Comment
