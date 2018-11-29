2be984d01b3466f50ba1888099dad400e470b24f
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 16:49:46 -0800

    Move photo components to picture folder renamed PhotoInfo to photos

---
+++ src/components/pictures/Photo.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+export default class Photo extends Component {
+
+  mediaType = (props) => {
+    if(props.mediaType === "video"){
+      return (
+        <iframe className="Home-Pic" width="540" height="500"
+          src={props.url} title={this.props.title}>
+        </iframe>
+      )
+    } else {
+    return (<img className="Home-Pic" src={this.props.url} alt={this.props.title} width="auto" height="550"/>)
+    }
+  }
+
+  render(){
+    return (
+      <div >
+         <h3>{this.props.title}</h3>
+         {this.mediaType(this.props)}
+      </div>
+    )
+  }
+}
---
+++ src/components/pictures/Photos.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Photo from './Photo'
+
+
+const Photos = props => {
+  if (props.photoInfo){
+    const renderPhoto = (<Photo key={1} mediaType={props.photoInfo.media_type} title={props.photoInfo.title} url={props.photoInfo.url} explanation={props.photoInfo.explanation}/>)
+
+    return (
+      <div>
+        {renderPhoto}
+      </div>
+    )
+  }
+
+}
+
+export default Photos
