5ff0b0a1646c4b808e94d9e8d23beb57266f7b46
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-28 21:22:33 -0800

    Add fetch all reducer and action add pictures array to reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 export default function rootsReducer(state = {
     isFetchingPhoto: false,
     picture: [],
+    pictures:[],
     comments: [],
     commentRequestInProgress: false
   }, action) {
@@ -14,7 +15,11 @@ export default function rootsReducer(state = {
     case 'ADD_COMMENT_TO_STATE':
       const comment = action.data
       return {...state, commentRequestInProgress: false, comments: state.comments.concat(comment) }
+    case 'ADD_ALL_PHOTOS_TO_STATE':
+      return {isFetchingPhoto: false, pictures: action.data}
+
     default:
-      return state;
+         return state;
+
   }
 }
