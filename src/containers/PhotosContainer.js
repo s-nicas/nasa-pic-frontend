d19327caf3208d0034062d8056fc9b6693dffb71
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-28 15:20:29 -0500

    Fix spacing

--- src/containers/PhotosContainer.js
+++ src/containers/PhotosContainer.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchPhoto } from '../actions/picActions'
-import Photos from '../components/pictures/Photos'
-import InformationPopOver from '../components/pictures/InformationPopOver'
-import CommentsContainer from './CommentsContainer'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPhoto } from '../actions/picActions';
+import Photos from '../components/pictures/Photos';
+import InformationPopOver from '../components/pictures/InformationPopOver';
+import CommentsContainer from './CommentsContainer';
 
 class PhotosContainer extends Component {
 
-  render(){
+  render() {
+
     return (
       <div>
-        {Object.keys(this.props.photo).length !== 0 ? <Photos photoInfo={this.props.photo} /> : false }
-        {Object.keys(this.props.photo).length !== 0 ? <InformationPopOver photo={this.props.photo}/> : false}
-        {Object.keys(this.props.photo).length !==0 ? <CommentsContainer/> : false }
+        { Object.keys( this.props.photo ).length !== 0 ? <Photos photoInfo={ this.props.photo } /> : false }
+        { Object.keys( this.props.photo ).length !== 0 ? <InformationPopOver photo={ this.props.photo } /> : false }
+        { Object.keys( this.props.photo ).length !== 0 ? <CommentsContainer/> : false }
       </div>
     )
   }
+  
 }
 
-function mapDispatchToProps(dispatch){
-  return { fetchPhoto: date => dispatch(fetchPhoto(date)) }
+function mapDispatchToProps( dispatch ){
+  return { fetchPhoto: date => dispatch( fetchPhoto(date) ) }
 }
 
-function mapStateToProps(state){
-  return {photo: state.picture}
+function mapStateToProps( state ){
+  return { photo: state.picture }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(PhotosContainer)
+
+export default connect( mapStateToProps, mapDispatchToProps )( PhotosContainer )
