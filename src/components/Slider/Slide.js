de7687a2581f16c2a4baac31cc6dd2af8e0cb095
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 11:03:20 -0500

    Remove title from Slide
    Not using functionality

--- src/components/Slider/Slide.js
+++ src/components/Slider/Slide.js
@@ -4,12 +4,13 @@ const Slide = ({ image }) => {
 
 
   const styles = {
-    backgroundImage: `url(${image})`,
+    backgroundImage: `url(${ image })`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: '50% 60%'
   }
-  return <div className="slide" style={styles}></div>
+
+  return <div className="slide" style={ styles }></div>
 }
 
 export default Slide
