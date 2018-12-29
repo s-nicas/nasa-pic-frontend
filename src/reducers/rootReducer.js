48e7c5f608fac7193846840838e9969bbe380db4
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 10:35:06 -0500

    Fix spacing

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,27 +1,30 @@
-export default function rootsReducer(state = {
+export default function rootsReducer( state = {
     isFetchingPhoto: false,
     picture: [],
     pictures:[],
     comments: [],
     commentRequestInProgress: false
-  }, action) {
-  switch (action.type) {
+  }, action ) {
+
+  switch ( action.type ) {
+
     case 'FETCH_PHOTO':
-      return {...state, isFetchingPhoto: true, picture: []}
+      return { ...state, isFetchingPhoto: true, picture: [] }
     case 'ADD_PHOTO_TO_STATE':
-      return {isFetchingPhoto: false, picture: action.data, comments: action.data.comments}
+      return { isFetchingPhoto: false, picture: action.data, comments: action.data.comments }
     case 'COMMENT_REQUEST':
-      return {...state, commentRequestInProgress: true}
+      return { ...state, commentRequestInProgress: true }
     case 'ADD_COMMENT_TO_STATE':
       const comment = action.data
-      return {...state, commentRequestInProgress: false, comments: state.comments.concat(comment) }
+      return { ...state, commentRequestInProgress: false, comments: state.comments.concat(comment) }
     case 'FETCH_ALL_PHOTOS':
-      return {isFetchingPhoto: true, pictures: []}
+      return { isFetchingPhoto: true, pictures: [] }
     case 'ADD_ALL_PHOTOS_TO_STATE':
-      return {isFetchingPhoto: false, pictures: action.data}
+      return { isFetchingPhoto: false, pictures: action.data }
 
     default:
-         return state;
+      return state;
 
   }
+
 }
