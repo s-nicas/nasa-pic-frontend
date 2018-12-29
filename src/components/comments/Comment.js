4830fa41882057ae32a10be6991d69ff952c0734
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 09:20:13 -0500

    Fix spacing
    Fix spacing  - no changes to code logic

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import LikeButton from './LikeButton';
 
 
-const Comment = (props) => {
+const Comment = ( props ) => {
   return (
       <div>
-          <p><b><i>{props.author}</i></b> says <i>"{props.content}"</i></p>
+          <p><b><i>{ props.author }</i></b> says <i>"{ props.content }"</i></p>
+          <LikeButton/>
       </div>
     )
   }
