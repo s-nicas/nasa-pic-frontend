65b65dd3e5f1d1dd5a5dabc3a6d7792cbd2d02aa
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 09:23:58 -0500

    Fix spacing
    Fix spacing - no changes to code logic

--- src/components/comments/CommentsForm.js
+++ src/components/comments/CommentsForm.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const CommentForm = props => {
   return (
-    <form id="commentsForm" onSubmit={props.handleOnCommentSubmit}>
-      <label >
-      By:
+    <form id="commentsForm" onSubmit={ props.handleOnCommentSubmit }>
+      <label>
+        By:
         <input type="text"
           name="author"
-          value={props.value}
-          onChange={props.handleAuthorOnChange}
+          value={ props.value }
+          onChange={ props.handleAuthorOnChange }
         />
       </label>
       <label>
         Content:
         <input type="text"
           name="content"
-          value={props.contentValue}
-          onChange={props.handleContentOnChange}/>
+          value={ props.contentValue }
+          onChange={ props.handleContentOnChange }/>
       </label>
       <input type="submit" value="Submit" />
     </form>
