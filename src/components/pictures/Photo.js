94236a72aa86f9b33f8010ae1b9577bcb37e083d
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 09:18:59 -0500

    Fix spacing
    Fix spacing no changes to code logic

--- src/components/pictures/Photo.js
+++ src/components/pictures/Photo.js
@@ -2,23 +2,25 @@ import React, {Component} from 'react'
 
 export default class Photo extends Component {
 
-  mediaType = (props) => {
-    if(props.mediaType === "video"){
+  mediaType = ( props ) => {
+    if( props.mediaType === "video" ){
       return (
         <iframe className="Home-Pic" width="540" height="500"
-          src={props.url} title={this.props.title}>
+          src={ props.url } title={ this.props.title }>
         </iframe>
       )
     } else {
-    return (<img className="Home-Pic" src={this.props.url} alt={this.props.title} width="auto" height="550"/>)
+      return (
+        <img className="Home-Pic" src={ this.props.url } alt={ this.props.title } width="auto" height="550"/>
+      )
     }
   }
 
-  render(){
+  render() {
     return (
-      <div >
-         <h3>{this.props.title}</h3>
-         {this.mediaType(this.props)}
+      <div>
+         <h3> { this.props.title } </h3>
+         { this.mediaType(this.props) }
       </div>
     )
   }
