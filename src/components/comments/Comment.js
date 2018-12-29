1d3166c586e954cae80375419e432174f9782c92
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-12-29 10:32:56 -0500

    Delete like button
    Like button has been removed as a feature. Only added it for testing purposes.

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import LikeButton from './LikeButton';
 
 
 const Comment = ( props ) => {
   return (
       <div>
           <p><b><i>{ props.author }</i></b> says <i>"{ props.content }"</i></p>
-          <LikeButton/>
       </div>
     )
   }
