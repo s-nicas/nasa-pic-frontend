7b977b629f6d1e1924242b8a362dd4e322a7bca8
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-01-04 16:14:41 -0800

    Update App to be stateless and add Switch
    App doesn't need to be aware of state. Add switch and removed exact path. Learned that exact path can create strange behaviors.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import NavBar from './components/NavBar';
+import PicSliderContainer from './containers/PicSliderContainer';
+import PhotosContainer from './containers/PhotosContainer';
+import SearchPhoto from './components/SearchPhoto';
 import './App.css';
-import PhotosContainer from './containers/PhotosContainer'
-import { BrowserRouter as Router, Route} from "react-router-dom"
-import Home from './containers/Home'
-import NavBar from './components/NavBar'
-import PicSliderContainer from './containers/PicSliderContainer'
 
 
-import SearchPhoto from './components/SearchPhoto'
-
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
     <Router>
       <div className="App">
-        <NavBar/>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/search" component={SearchPhoto}/>
-        <Route exact path="/photos/:date" component={PhotosContainer}/>
-        <Route exact path="/photos" component={PicSliderContainer} />
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={SearchPhoto} />
+          <Route exact path="/photos/:date" component={PhotosContainer} />
+          <Route exact path="/photos" component={PicSliderContainer} />
+        </Switch>
       </div>
     </Router>
-    );
-  }
-}
+  );
+};
 
 export default App;
