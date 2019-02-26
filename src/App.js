83e5d153c57276ed9126fae34f734f66e8e5c716
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-02-25 18:21:19 -0800

    Add css styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './containers/Home';
-import NavBar from './components/NavBar';
-import PicSliderContainer from './containers/PicSliderContainer';
-import PhotosContainer from './containers/PhotosContainer';
-import SearchPhoto from './components/SearchPhoto';
+import React, { Component } from 'react';
+import './App.css';
+import PhotosContainer from './containers/PhotosContainer'
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import Home from './containers/Home'
+import NavBar from './components/NavBar'
+import PicSliderContainer from './containers/PicSliderContainer'
 
 
-const App = () => {
-  return (
+import SearchPhoto from './components/SearchPhoto'
+
+class App extends Component {
+  render() {
+    return (
     <Router>
       <div className="App">
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/search" component={SearchPhoto} />
-          <Route exact path="/photos/:date" component={PhotosContainer} />
-          <Route exact path="/photos" component={PicSliderContainer} />
-        </Switch>
+        <NavBar/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/search" component={SearchPhoto}/>
+        <Route exact path="/photos/:date" component={PhotosContainer}/>
+        <Route exact path="/photos" component={PicSliderContainer} />
       </div>
     </Router>
-  );
-};
+    );
+  }
+}
 
 export default App;
