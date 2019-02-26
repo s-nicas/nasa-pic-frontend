e8f8ca0c7cb29f89d6847145c0ff9bab4936ffdc
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-02-25 17:43:33 -0800

    Add Switch from react-router-dom
    Adds an additional control over routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PhotosContainer from './containers/PhotosContainer'
-import { BrowserRouter as Router, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Home from './containers/Home'
 import NavBar from './components/NavBar'
 import PicSliderContainer from './containers/PicSliderContainer'
@@ -13,13 +13,15 @@ class App extends Component {
   render() {
     return (
     <Router>
-      <div className="App">
-        <NavBar/>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/search" component={SearchPhoto}/>
-        <Route exact path="/photos/:date" component={PhotosContainer}/>
-        <Route exact path="/photos" component={PicSliderContainer} />
-      </div>
+      <Switch>
+        <div className="App">
+          <NavBar/>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/search" component={SearchPhoto}/>
+          <Route exact path="/photos/:date" component={PhotosContainer}/>
+          <Route exact path="/photos" component={PicSliderContainer} />
+        </div>
+      </Switch>
     </Router>
     );
   }
