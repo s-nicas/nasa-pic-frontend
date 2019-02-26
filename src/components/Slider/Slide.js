ab8bd7b902ddf62ade1a2df8afaaf350ef36dba6
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-02-25 17:17:20 -0800

    Fix spacing
    No change to logic

--- src/components/Slider/Slide.js
+++ src/components/Slider/Slide.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const Slide = ({ image }) => {
+const Slide = (props) => {
 
   const styles = {
-    backgroundImage: `url(${ image })`,
+    backgroundImage: `url(${ props.image })`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: '50% 60%'
   }
 
   return <div className="slide" style={ styles }></div>
+
 }
 
 export default Slide
